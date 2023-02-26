f9624a0ee684419cecf3ef5a30293d08c512c40c
TaliyaGaleeva <126391765+TaliyaGaleeva@users.noreply.github.com>
2023-02-26 14:44:46 +0300

    New v1

---
+++ script.js
@@ -0,0 +1,94 @@
+// const skill = document.getElementById('skill')
+// const isLove = document.getElementById('isLove')
+// const string = document.getElementById('string')
+
+// // const skillText = prompt("Какой язык Вы учите?", "я не в курсе!")
+// // const isLoveValue = confirm('Вы любите изучамый язык?')
+
+
+// // skill.innerText = skillText
+// // isLove.innerText = isLoveValue
+// string.innerHTML = "str "    
+
+// let a = "seven"
+// a = 7
+// const b = 8
+
+//STRING
+// const str1 = 'I\'m OK'
+// const str2 = "Text \ntext"
+// const str3 = `I'm say ${str1}`
+
+// console.log(str1)
+// console.log(str2)
+// console.log(str3) 
+
+//NUMBER
+//от -(2**53 -1) до (2**53 -1)
+// console.log (9999999999999999)
+// // + - * / **
+// const num1 = 75 + 15
+// const num2 = 79**3
+// const rem = 7 + 8 * "a" 
+//NaN Not a Number
+const inf = 576 / "8"
+// alert(num2)
+console.log(inf)
+
+
+// BIGINT
+// const bigint = 99999999999999999999999999999n //n обозначение большого числа
+// console.log(bigint)
+
+//BOOLEAN
+//true / false
+// > < >= <= >== <== ==
+// const bool1 = 10  > 5
+// console.log(bool1)
+// const bool2 = "a"  > "AAA" //т.к. кодировка UTF-8 
+// console.log(bool2)
+
+// // //NULL
+// let empty = null
+// console.log(typeof empty)
+
+// // //UNDEFINED
+// let box 
+// console.log(box, typeof box)
+
+//SYMBOL
+// const uniq = Symbol('id')
+// const uniq2 = Symbol('id')
+// console.log(uniq == uniq2)
+
+//OBJECT
+
+// const obj = {
+//     name: "Sasha",
+//     age: 19,
+//     isHappy: true,
+// }
+// // console.log(obj.name)
+// // console.log(obj['name'])
+
+// obj.job = "Google"
+// // console.log(obj)
+
+// const arrey = ["Masha",18, false]
+// arrey[3] = "Facebook"
+// console.log(arrey)
+// console.log(arrey[0])
+
+let count = 15
+
+let point = 5
+
+while (count < 18) {
+
+point = point + 3
+
+count++
+
+}
+
+console.log(point)
